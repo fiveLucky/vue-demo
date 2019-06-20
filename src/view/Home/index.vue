4ab4cb787d705af528bd98364899bdfa99a2981a
wenjie.xu <wenjie.xu@bianlifeng.com>
2019-06-20 14:54:30 +0800

    update

--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -3,6 +3,13 @@
   <div>
     <div :class="styles.title">{{msg}}</div>
     <slot></slot>
+    <input placeholder="text" @input="input" v-model="model.input">
+    <select name="s" id="s" v-model="model.select" @change="changeInput">
+      <option value="1">1-a</option>
+      <option value="2">2-a</option>
+      <option value="3">3-a</option>
+    </select>
+    <div>wwwww{{model}}</div>
   </div>
 </template>
 
@@ -14,7 +21,21 @@ export default {
   props: {
     msg: String
   },
-  data: () => ({ styles })
+  data: () => ({
+    styles,
+    model: {
+      input: "",
+      select: "1"
+    }
+  }),
+  methods: {
+    changeInput(arg) {
+      console.log(arg, "change");
+    },
+    input(arg) {
+      console.log(arg);
+    }
+  }
 };
 </script>
 
